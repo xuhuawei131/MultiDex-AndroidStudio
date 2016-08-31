76959e4b348d347474893aabf02c15ee31d4166d
yubo <yubo@listome.com>
2016-08-31 17:58:58 +0800

    first commit

---
+++ app/src/main/java/com/test/multidexdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.test.multidexdemo;
+
+import android.content.Intent;
+import android.support.multidex.MultiDex;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import dalvik.system.DexFile;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void btnClick(View view) {
+        startActivity(new Intent(this, OtherActivity.class));
+    }
+}
